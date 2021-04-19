d1a8e4afe531e35bc20532661411f11bb9bf93d7
FN-Florian <ninow@fn-systems.eu>
2021-04-19 21:37:29 +0200

    ChangeAzureADUserPasswordFromCSV

---
+++ ChangeAzureADUserPasswordFromCSV.ps1
@@ -0,0 +1,35 @@
+#ScriptRoot
+if ($PSVersionTable.PSVersion.Major -gt 2) {
+$ScriptRoot = $PSScriptRoot
+}
+else {
+$ScriptRoot = split-path -parent $MyInvocation.MyCommand.Definition
+}
+
+
+
+Install-Module -Name AzureAD
+
+Get-Module AzureAD
+Connect-AzureAD
+
+
+
+$UserResetList = Import-Csv -Path "$ScriptRoot/ExampleFiles/UserPasswordResetList.csv" -Delimiter ";"
+
+
+$UserResetList
+
+
+$i =0
+while ($i -lt $UserResetList.length){
+
+    $password = ConvertTo-SecureString $UserResetList[$i].Password -AsPlainText -Force
+
+    Set-AzureADUserPassword -ObjectId $UserResetList[$i].ObjectID -Password $password
+    
+
+    Write-Host -ForegroundColor Yellow "SetPassword for "$UserResetList[$i].ObjectID
+
+    $i++;
+}
